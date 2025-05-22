d74feb2a1919114b7747e88c2323f6fdf52deb77
OminduKumara <IT23575776@my.sliit.lk>
2025-05-22 11:24:08 +0530

    Initial commit

--- src/components/AdminFirmware.jsx
+++ src/components/AdminFirmware.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { isAuthenticated, isAdmin } from '../services/UserService';
-import { getAllFirmware, uploadFirmware, deleteFirmware } from '../api';
+import { getAllFirmware, uploadFirmware, deleteFirmware } from '../services/FirmwareService';
 import './AdminFirmware.css';
 
 const AdminFirmware = () => {
