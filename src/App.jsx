e6dd01656e336b495242c7c09119f8c770e48291
OminduKumara <IT23575776@my.sliit.lk>
2025-05-25 14:14:02 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navibar from './components/Navibar';
 import Footer from './components/Footer';
 import './App.css';
-import { useEffect } from 'react';
 
 import Home from './components/Home';
 import Products from './components/Products';
@@ -26,120 +25,87 @@ import UpdateUser from './components/userspage/UpdateUser';
 import CreateProduct from './pages/CreateProduct';
 import EditProduct from './pages/EditProduct';
 
-// Protected Route component with better error handling
+// Protected Route component
 const ProtectedRoute = ({ children, requireAdmin = false }) => {
     if (!isAuthenticated()) {
-        return <Navigate to="/login" replace state={{ message: "Please login to access this page" }} />;
+        return <Navigate to="/login" />;
     }
 
     if (requireAdmin && !isAdmin()) {
-        return <Navigate to="/" replace state={{ message: "Admin access required" }} />;
+        return <Navigate to="/" />;
     }
 
     return children;
 };
 
 function App() {
-    useEffect(() => {
-        document.title = 'CIROMOBILES';
-    }, []);
-
     return (
         <div className="app-wrapper">
             <Navibar />
             <main className="main-content">
                 <Routes>
-                    {/* Public Routes */}
                     <Route path='/' element={<Home />} />
                     <Route path='/products' element={<Products />} />
                     <Route path='/product/:id' element={<ProductDetails />} />
                     <Route path='/repair' element={<EnterNumberPage />} />
                     <Route path='/repair_tracking/display_progress_file/display_progress_page' element={<DisplayProgressPage />} />
+                    <Route path='/repair_tracking/progress_update_file/progress_update_page' element={
+                        <ProtectedRoute requireAdmin={true}>
+                            <ProgressUpdatePage />
+                        </ProtectedRoute>
+                    } />
                     <Route path='/aboutus' element={<Aboutus />} />
+                    <Route path='/productlist' element={
+                        <ProtectedRoute requireAdmin={true}>
+                            <ProductList />
+                        </ProtectedRoute>
+                    } />
+                    <Route path='/create-product' element={
+                        <ProtectedRoute requireAdmin={true}>
+                            <CreateProduct />
+                        </ProtectedRoute>
+                    } />
+                    <Route path='/edit-product/:id' element={
+                        <ProtectedRoute requireAdmin={true}>
+                            <EditProduct />
+                        </ProtectedRoute>
+                    } />
                     <Route path='/firmware' element={<Firmware />} />
                     <Route path='/faq' element={<FAQList />} />
                     <Route path='/login' element={<LoginPage />} />
-
-                    {/* Admin Routes */}
-                    <Route path='/repair_tracking/progress_update_file/progress_update_page' 
-                        element={
-                            <ProtectedRoute requireAdmin={true}>
-                                <ProgressUpdatePage />
-                            </ProtectedRoute>
-                        } 
-                    />
-                    <Route path='/productlist' 
-                        element={
-                            <ProtectedRoute requireAdmin={true}>
-                                <ProductList />
-                            </ProtectedRoute>
-                        } 
-                    />
-                    <Route path='/create-product' 
-                        element={
-                            <ProtectedRoute requireAdmin={true}>
-                                <CreateProduct />
-                            </ProtectedRoute>
-                        } 
-                    />
-                    <Route path='/edit-product/:id' 
-                        element={
-                            <ProtectedRoute requireAdmin={true}>
-                                <EditProduct />
-                            </ProtectedRoute>
-                        } 
-                    />
-                    <Route path='/admin/firmware' 
-                        element={
-                            <ProtectedRoute requireAdmin={true}>
-                                <AdminFirmware />
-                            </ProtectedRoute>
-                        } 
-                    />
-                    <Route path='/firmwaremanager' 
-                        element={
-                            <ProtectedRoute requireAdmin={true}>
-                                <FirmwareManager />
-                            </ProtectedRoute>
-                        } 
-                    />
-                    <Route path='/admin/faq' 
-                        element={
-                            <ProtectedRoute requireAdmin={true}>
-                                <AdminFAQList />
-                            </ProtectedRoute>
-                        } 
-                    />
-                    <Route path='/register' 
-                        element={
-                            <ProtectedRoute requireAdmin={true}>
-                                <RegistrationPage />
-                            </ProtectedRoute>
-                        } 
-                    />
-                    <Route path='/admin/user-management' 
-                        element={
-                            <ProtectedRoute requireAdmin={true}>
-                                <UserManagementPage />
-                            </ProtectedRoute>
-                        } 
-                    />
-                    <Route path='/update-user/:userId' 
-                        element={
-                            <ProtectedRoute requireAdmin={true}>
-                                <UpdateUser />
-                            </ProtectedRoute>
-                        } 
-                    />
-
-                    {/* User Routes */}
-                    <Route path='/profile' 
-                        element={
-                            <ProtectedRoute>
-                                <ProfilePage />
-                            </ProtectedRoute>
-                        } 
-                    />
+                    <Route path='/firmwaremanager' element={<FirmwareManager />} />
+                    <Route path='/admin/firmware' element={
+                        <ProtectedRoute requireAdmin={true}>
+                            <AdminFirmware />
+                        </ProtectedRoute>
+                    } />
+                    
+                    {/* Protected Routes */}
+                    <Route path='/profile' element={
+                        <ProtectedRoute>
+                            <ProfilePage />
+                        </ProtectedRoute>
+                    } />
+                    <Route path='/register' element={
+                        <ProtectedRoute requireAdmin={true}>
+                            <RegistrationPage />
+                        </ProtectedRoute>
+                    } />
+                    <Route path='/admin/faq' element={
+                        <ProtectedRoute requireAdmin={true}>
+                            <AdminFAQList />
+                        </ProtectedRoute>
+                    } />
+                    <Route path='/admin/user-management' element={
+                        <ProtectedRoute requireAdmin={true}>
+                            <UserManagementPage />
+                        </ProtectedRoute>
+                    } />
+                    <Route path='/update-user/:userId' element={
+                        <ProtectedRoute requireAdmin={true}>
+                            <UpdateUser />
+                        </ProtectedRoute>
+                    } />
                 </Routes>
             </main>
             <Footer />
