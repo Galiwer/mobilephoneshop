8275f365f55f775c02cf70e7a4ffcc13e5914d58
Omindu <108943596+Galiwer@users.noreply.github.com>
2025-07-02 19:00:18 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const RAILWAY_URL = "https://graceful-strength-production-360f.up.railway.app";
+const RAILWAY_URL = "https://backendmobilephone-production.up.railway.app/";
 
 export const API_BASE_URL = import.meta.env.VITE_API_URL || RAILWAY_URL;
 
